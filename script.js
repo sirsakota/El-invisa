cb42f7a91467a39597a0244a5960051cfa78da09
Mystic <mystic189123@gmail.com>
2025-02-04 10:21:21 +0200

    Script

---
+++ script.js
@@ -0,0 +1,13 @@
+const animals = {
+  Data: [
+    {
+      Name: "Karhu",
+      Info: "ruskea, karvainen, nukkuu talvet",
+      Hints: ["Asuu metsässä", "tassuissa kynnet"],
+    },
+  ],
+};
+
+for (let i = 0; i < animals.Data.length; i++) {
+  console.log(animals.Data[i]);
+}
